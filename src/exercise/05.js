613db893415d9672f370c690ece8cf81d16e10f3
malekabbess <malek.abbes@bearstudio.fr>
2023-06-21 17:54:12 +0100

    React fundamentals Exercices + all extra done :rocket:

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,16 +14,65 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// const smallBox = (
+//   <div
+//     className="box--small"
+//     style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
+//   >
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = (
+//   <div
+//     className="box--medium"
+//     style={{fontStyle: 'italic', backgroundColor: 'pink'}}
+//   >
+//     medium pink box
+//   </div>
+// )
+// const largeBox = (
+//   <div
+//     className="box--large"
+//     style={{fontStyle: 'italic', backgroundColor: 'orange'}}
+//   >
+//     large orange box
+//   </div>
+// )
 
+// Extra 1
+// const Box = ({style, className = '', ...otherProps}) => {
+//   return (
+//     <div
+//       className={`box ${className}`}
+//       style={{fontStyle: 'italic', ...style}}
+//       {...otherProps}
+//     />
+//   )
+// }
+// Extra 2
+const Box = ({style, size, ...otherProps}) => {
+  const sizeClass = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${sizeClass}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
+// Extra 2
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,7 +1,7 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import * as React from 'react'
+import React, {useRef, useState} from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
@@ -14,18 +14,47 @@ function UsernameForm({onSubmitUsername}) {
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
   // 🐨 Call `onSubmitUsername` with the value of the input
+  const [error, setError] = useState(null)
+  const [IsLowercase, SetIsLowercase] = useState(false)
+  const [username, setUsername] = useState()
+  const userInputRef = useRef(null)
+  const handleSubmit = event => {
+    event.preventDefault()
+    
+    // onSubmitUsername(userInputRef.current.value)
 
+    // Extra 3
+    onSubmitUsername(username)
+  }
+  const HandleChange = e => {
+    // Extra 2
+    // const {value} = e.target // RQ : {value}=e.target =! value=e.target
+    // value === value.toLowerCase() ? SetIsLowercase(true) : SetIsLowercase(false)
+    // setError(IsLowercase ? null : 'Votre username doit etre en miniscule')
+    setUsername(e.target.value.toLowerCase())
+  }
   // 🐨 add the onSubmit handler to the <form> below
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label>Username:</label>
-        <input type="text" />
+        <input
+          type="text"
+          name="nameInput"
+          onChange={HandleChange}
+          ref={userInputRef}
+          value={username}
+        />
+        <div role="alert" style={{color: 'red', fontWeight: 'bold'}}>
+          {error}
+        </div>
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={error} type="submit">
+        Submit
+      </button>
     </form>
   )
 }
